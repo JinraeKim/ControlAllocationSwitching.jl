ad3939007ddcbf7d205ff2b24a485556aa081dff
JinraeKim <kjl950403@gmail.com>
2021-08-26 01:14:05 +0900

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.17

---
+++ src/ControlAllocationSwitching.jl
@@ -0,0 +1,5 @@
+module ControlAllocationSwitching
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ControlAllocationSwitching
+using Test
+
+@testset "ControlAllocationSwitching.jl" begin
+    # Write your tests here.
+end
